32756a5e537a50d2d592dc6a6f194b766865655c
Albert-Dias <albert.uft@gmail.com>
2019-10-22 06:41:02 -0300

    guardando projeto finalizado

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import New from './pages/New';
+
+export default function Routes(){
+    return(
+        <BrowserRouter>
+            <Route path="/" exact component={Login} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/new" component={New} />
+        </BrowserRouter>
+    )
+}
